6f78d7f4fd1fa7f809d78c9ee10501273ee1eecf
AakashDayma01 <daymaaakash334@gmail.com>
2026-07-07 16:53:04 +0530

    solved errors from models

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -13,44 +13,42 @@ class CustomUser(AbstractUser):
         ('O', 'Other'),
     ]
     full_name = models.CharField(max_length=100, blank=True)
-    profile_pic = models.ImageField(upload_to='profile_pic/', null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     date_of_birth = models.DateField(null=True, blank=True)
-    bio = models.TextField(max_length=0, blank=False)
-    website = models.URLField(max_length=2, blank=True)
+    bio = models.TextField(max_length=150, blank=True)
+    website = models.URLField(blank=True)
     phone_number = models.CharField(max_length=15, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    following = models.ManyToManyField(
-        'self', through='Contact', related_name='followers', symmetrical=False
-    )
-
-
+    # following = models.ManyToManyField(
+    #     'self', through='Contact', related_name='followers', symmetrical=False
+    # )
     def __str__(self):
         return self.username
     
 
-class Contact(models.Model):
-    user_from = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        related_name='rel_from_set', 
-        on_delete=models.CASCADE
-    )
-    user_to = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        related_name='rel_to_set', 
-        on_delete=models.CASCADE
-    )
-    created = models.DateTimeField(auto_now_add=True)
+# class Contact(models.Model):
+#     user_from = models.ForeignKey(
+#         settings.AUTH_USER_MODEL, 
+#         related_name='rel_from_set', 
+#         on_delete=models.CASCADE
+#     )
+#     user_to = models.ForeignKey(
+#         settings.AUTH_USER_MODEL, 
+#         related_name='rel_to_set', 
+#         on_delete=models.CASCADE
+#     )
+#     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ('-created',)
-        constraints = [
-            models.UniqueConstraint(fields=['user_from', 'user_to'], name='unique_followers')
-        ]
+#     class Meta:
+#         ordering = ('-created',)
+#         constraints = [
+#             models.UniqueConstraint(fields=['user_from', 'user_to'], name='unique_followers')
+#         ]
 
-    def __str__(self):
-        return f'{self.user_from} follows {self.user_to}'
+#     def __str__(self):
+#         return f'{self.user_from} follows {self.user_to}'
 
 
 
@@ -67,3 +65,5 @@ class PasswordResetOTP(models.Model):
         cls.objects.filter(user=user).delete()
         otp_code = f"{random.randint(100000, 999999)}"
         return cls.objects.create(user=user, otp=otp_code)
+
+
